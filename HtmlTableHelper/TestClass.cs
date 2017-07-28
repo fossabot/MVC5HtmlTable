7cc9f842e524e14eda6ac41b989f2138723a7eeb
Mathieu <mathieu@ctoutvert.com>
2017-07-28 15:56:05 +0200

    Add first working test helper

---
+++ HtmlTableHelper/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using RazorEngine;
+using RazorEngine.Templating;
+
+namespace HtmlTableHelper
+{
+    public static class TestClass
+    {
+        public static string Testhelper(this HtmlHelper helper)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(Engine.Razor.RunCompile("Hello @Model.Name", "templateKey", null, new { Name = "test" }));
+            return builder.ToString();
+        }
+    }
+}
