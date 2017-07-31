6978eaebf65e4931021998420ec95fc9ddc6004b
Mathieu <mathieu@ctoutvert.com>
2017-07-31 13:02:33 +0200

    Make class generic

--- HtmlTableHelper/HelperContainer.cs
+++ HtmlTableHelper/HelperContainer.cs
@@ -1,22 +1,22 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.UI.HtmlControls;
 using RazorEngine.Compilation.ImpromptuInterface.Dynamic;
 
 namespace HtmlTableHelper
 {
     public static class TableHelpers
     {
-
-        public static HtmlTable DisplayTable<TModel, TValue>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TValue>> expression)
+        public static HtmlTable<TValue> DisplayTable<TModel, TValue>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TValue>> expression) where TValue : IEnumerable
         {
             Func<TModel, TValue> deleg = expression.Compile();
             var result = deleg(helper.ViewData.Model);
-            //to do
-            //return new MvcHtmlString(result.ToString());
 
-            return new HtmlTable();
+            return new HtmlTable<TValue>(result);
         }
     }
 }
--- HtmlTableHelper/HtmlTable.cs
+++ HtmlTableHelper/HtmlTable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 using HtmlTableHelper.ViewModel;
@@ -8,18 +9,27 @@ using RazorEngine.Templating;
 
 namespace HtmlTableHelper
 {
-    public class HtmlTable
+    public class HtmlTable<TValue>
     {
+        private TValue _model;
         private StringBuilder _str = new StringBuilder();
         private static readonly string ViewsPath = Path.Combine(new Uri(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase) ?? "").LocalPath, "Views");
 
-        public HtmlTable()
+        public HtmlTable(TValue model)
         {
-            _str.Append(Engine.Razor.RunCompile(File.ReadAllText($"{ViewsPath}/Table.cshtml"), "table", null, new TableViewModel()));
+            Init(model);
+        }
+
+        public HtmlTable<TValue> Init(TValue model)
+        {
+            _model = model;
+
+            return this;
         }
 
         public string Render()
         {
+            _str.Append(Engine.Razor.RunCompile(File.ReadAllText($"{ViewsPath}/Table.cshtml"), "table", null, new TableViewModel()));
             return _str.ToString();
         }
     }
