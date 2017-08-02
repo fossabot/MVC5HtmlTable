8d7b00a0257374695485123c0120a9959cad6fc5
Mathieu <mathieu@ctoutvert.com>
2017-08-02 17:59:48 +0200

    Add different types for example

--- HtmlHelperUtilisation/Models/RowViewModel.cs
+++ HtmlHelperUtilisation/Models/RowViewModel.cs
@@ -5,5 +5,7 @@
         public string Col1 { get; set; } = "Col1Value";
         public string Col2 { get; set; } = "Col2Value";
         public string Col3 { get; set; } = "Col3Value";
+        public long Col4 { get; set; } = 18;
+        public bool Col5 { get; set; } = false;
     }
 }
